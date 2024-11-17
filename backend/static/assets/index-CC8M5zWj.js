fdef377170cb6f4b72c1440b2b4ea8b30b00264d
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2024-11-17 11:59:15 +0300

    [frontend]: updated something

---
+++ backend/static/assets/index-CC8M5zWj.js
@@ -0,0 +1,1 @@
+import{E as o}from"./index-DBdZu_x3.js";const e=o;export{e as component};
---
+++ backend/static/assets/login-_ssf7mv6.js
@@ -0,0 +1,1 @@
+import{j as o,a as s}from"./index-DBdZu_x3.js";const a=()=>o.jsx(s,{});export{a as component};
